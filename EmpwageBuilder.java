f5eeec256e14a85abb6a5671910ede437ffa47c3
0vishal <vishalsalaskar72@gmail.com>
2021-02-24 02:17:59 +0530

    Daily Employee Wage

--- EmpwageBuilder.java
+++ EmpwageBuilder.java
@@ -1,16 +1,11 @@
 public class EmpwageBuilder {
 
         public static void main(String[] args) {
-            // write your code here
-            System.out.println("Welcome to Employee wage computation problem on master branch");
-            int IS_FULLTIME = 1;
-            double empcheck = Math.floor(Math.random() * 10) % 2;
 
-            if (empcheck == IS_FULLTIME) {
-                System.out.println("Employee is present");
-            } else {
-                System.out.println("Employee is absent");
-            }
+         int EMPWAGEPERHOUR = 20;
+         int EMPDAILYHOUR = 8;
+         int DAILYWAGE = (EMPDAILYHOUR * EMPWAGEPERHOUR);
+         System.out.println("Daily wage is " + DAILYWAGE);
         }
     }
 
