8fb5c86d07cc168a533db615b6042f71fcb8e83a
0vishal <vishalsalaskar72@gmail.com>
2021-02-24 02:50:58 +0530

    Add Part Time wage

--- EmpwageBuilder.java
+++ EmpwageBuilder.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class EmpwageBuilder {
 
-        public static void main(String[] args) {
+    public static void main(String args[]) {
+        int EMPWAGEPERHOUR = 20;
+        int EMPTIME = 0;
+        System.out.println("Calculate wage 1.FullTime 2.PartTime");
+        Scanner scan = new Scanner(System.in);
+        int TIME = scan.nextInt();
 
-         int EMPWAGEPERHOUR = 20;
-         int EMPDAILYHOUR = 8;
-         int DAILYWAGE = (EMPDAILYHOUR * EMPWAGEPERHOUR);
-         System.out.println("Daily wage is " + DAILYWAGE);
+        if(TIME == 1) {
+            EMPTIME = 8;
         }
+        else {
+            EMPTIME = 4;
+        }
+        int DAILYWAGE = (EMPTIME * EMPWAGEPERHOUR);
+        System.out.println("Daily wage is " + DAILYWAGE);
     }
-
+}
