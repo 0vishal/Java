e1f1c07cf26dba40da7d4fb95d84942831a1f99d
0vishal <vishalsalaskar72@gmail.com>
2021-02-24 02:57:30 +0530

    Swtich case

--- EmpwageBuilder.java
+++ EmpwageBuilder.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 
 public class EmpwageBuilder {
 
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         int EMPWAGEPERHOUR = 20;
         int EMPTIME = 0;
         System.out.println("Calculate wage 1.FullTime 2.PartTime");
         Scanner scan = new Scanner(System.in);
         int TIME = scan.nextInt();
 
-        if(TIME == 1) {
-            EMPTIME = 8;
+        switch (TIME) {
+            case 1:
+                EMPTIME = 8;
+            case 2:
+                EMPTIME = 4;
+
         }
-        else {
-            EMPTIME = 4;
-        }
+
         int DAILYWAGE = (EMPTIME * EMPWAGEPERHOUR);
         System.out.println("Daily wage is " + DAILYWAGE);
     }
