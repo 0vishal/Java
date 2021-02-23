69f63a61b091146e7e0ab1a3ba3a5ca55a620cab
0vishal <vishalsalaskar72@gmail.com>
2021-02-24 03:02:15 +0530

    Calculate Montly wage

--- EmpwageBuilder.java
+++ EmpwageBuilder.java
@@ -2,23 +2,26 @@ import java.util.Scanner;
 
 public class EmpwageBuilder {
 
-    public static void main(String args[])
-    {
-        int EMPWAGEPERHOUR = 20;
-        int EMPTIME = 0;
-        System.out.println("Calculate wage 1.FullTime 2.PartTime");
-        Scanner scan = new Scanner(System.in);
-        int TIME = scan.nextInt();
+    public static void main(String args[]) {
+            int EMPWAGEPERHOUR = 20;
+            int EMPTIME = 0;
+            int DAYS_IN_MONTH = 20;
 
-        switch (TIME) {
-            case 1:
-                EMPTIME = 8;
-            case 2:
-                EMPTIME = 4;
+            System.out.println("Calculate wage 1.FullTime 2.PartTime");
+            Scanner scan = new Scanner(System.in);
+            int TIME = scan.nextInt();
 
+            switch (TIME) {
+                case 1:
+                    EMPTIME = 8;
+                case 2:
+                    EMPTIME = 4;
+
+            }
+
+            int DAILYWAGE = (EMPTIME * EMPWAGEPERHOUR);
+            int MONTHLYWAGE = (DAYS_IN_MONTH * DAILYWAGE);
+
+            System.out.println("Monthly wage is " + MONTHLYWAGE);
         }
-
-        int DAILYWAGE = (EMPTIME * EMPWAGEPERHOUR);
-        System.out.println("Daily wage is " + DAILYWAGE);
-    }
 }
